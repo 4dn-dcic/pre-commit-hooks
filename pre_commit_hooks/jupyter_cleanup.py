d16366f830532a14658c5e450dd14474a4276a96
Koray Kırlı <koray_kirli@hms.harvard.edu>
2019-04-05 14:37:56 -0400

    add jupyter check

--- pre_commit_hooks/jupyter_cleanup.py
+++ pre_commit_hooks/jupyter_cleanup.py
@@ -1,13 +1,31 @@
 from __future__ import print_function
 
 import argparse
-import os
 import json
 import sys
 
 
-def parse_for_output(file_name):
-    return('atlar')
+def look_for_output_and_delete(file_name):
+    updated = False
+    with open(file_name) as json_file:
+        try:
+            content = json.load(json_file)
+        except:
+            return('not json compatible')
+        # iterate over cells and clear output
+        for a_cell in content['cells']:
+            if a_cell.get('outputs'):
+                updated = True
+                a_cell['outputs'] = []
+            if a_cell.get('execution_count'):
+                updated = True
+                a_cell['execution_count'] = None  # to be converted to null
+    if updated:
+        with open(file_name, 'w') as json_file:
+            json.dump(content, json_file)
+        return True
+    else:
+        return False
 
 
 def main(argv=None):
@@ -16,7 +34,10 @@ def main(argv=None):
     parser.add_argument('filenames', nargs='*', help='Filenames to run on')
     args = parser.parse_args(argv)
     for filename in args.filenames:
-        res = parse_for_output(filename)
+        res = look_for_output_and_delete(filename)
+        if res == 'not json compatible':
+            print('\nCAUTION')
+            print('{} can not be opened as json'.format(filename))
         if res:
             print('{} output has been cleared'.format(filename))
             return_code = 1
